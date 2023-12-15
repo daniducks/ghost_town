0bbd3c06b08fe9c2212edaa884f1170393920b81
daniducks <130884991+daniducks@users.noreply.github.com>
2023-12-15 11:38:46 +0800

    start btn works

---
+++ Ghost-Town/Assets/Scripts/StartScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+
+    public GameObject HighScore;
+    public GameObject startBtn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        startBtn.GetComponent<Button>().onClick.AddListener(startGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void startGame(){
+        SceneManager.LoadScene("SampleScene");
+    }
+}
