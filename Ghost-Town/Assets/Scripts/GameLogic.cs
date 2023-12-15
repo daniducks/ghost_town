5a03583eba496dacb51912e5852d2a42066dcf97
daniducks <130884991+daniducks@users.noreply.github.com>
2023-12-15 17:13:29 +0800

    timer ish

--- Ghost-Town/Assets/Scripts/GameLogic.cs
+++ Ghost-Town/Assets/Scripts/GameLogic.cs
@@ -6,15 +6,19 @@ public class GameLogic : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI Timer; 
     [SerializeField] private TextMeshProUGUI Score; 
+    [SerializeField] private float timeRemaining;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeRemaining = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeRemaining > 0){
+            Timer.text = timeRemaining.ToString();
+            timeRemaining -= Time.deltaTime;
+        }
     }
 }
